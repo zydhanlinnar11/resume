e9d4f13e0846c02a7144660e49d889d496cfc912
Zydhan Linnar Putra <zydhan.19051@mhs.its.ac.id>
2021-02-11 03:48:29 +0700

    Delete unnecessary files, and add new features
    - Delete LBE files
    - Add new logo and favicon
    - Add facebook and instagram

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
 navButtonPressed();
 let sidebarOpened = false;
 
+window.onresize = () => {
+  hideSideBar();
+}
+
 function navButtonPressed() {
+  setSosmedLink("navbar");
+  setSosmedLink("sidebar");
+  document.getElementById("navbar-sosmed").onclick = () => {
+    if(sidebarOpened)
+      hideSideBar();
+    else
+      showSideBar();
+  }
+}
+
+function hideSideBar() {
+  document.getElementById("sidebar").style["margin-left"] = "100%";
+  sidebarOpened = false;
+}
+
+function showSideBar() {
+  document.getElementById("sidebar").style["margin-left"] = "0%";
+  sidebarOpened = true;
+}
+
+function setSosmedLink(position) {
   let userName = "zydhanlinnar11";
   const linkSosmed = {
-    "twt" : "twitter.com",
-    "gh" : "github.com",
-    "fb" : "facebook.com",
-    "ig" : "instagram.com",
+    "twitter" : "twitter.com",
+    "github" : "github.com",
+    "facebook" : "facebook.com",
+    "instagram" : "instagram.com",
     "linkedin" : "linkedin.com/in"
   }
-  document.getElementById("linkedin").onclick = () => {window.open("https://" + linkSosmed["linkedin"] + "/" + userName)};
-  document.getElementById("gh").onclick = () => {window.open("https://" + linkSosmed["gh"] + "/" + userName)};
-  document.getElementById("sosmed").onclick = () => {
-    if(sidebarOpened) {
-      document.getElementById("sidebar").style["margin-left"] = "100%";
-    } else {
-      document.getElementById("sidebar").style["margin-left"] = "0%";
-    }
-    sidebarOpened = !sidebarOpened;
-  }
-  document.getElementById("linkedin-sidebar").onclick = () => {window.open("https://" + linkSosmed["linkedin"] + "/" + userName)};
-  document.getElementById("gh-sidebar").onclick = () => {window.open("https://" + linkSosmed["gh"] + "/" + userName)};
-  }
-
-  window.onresize = () => {
-    sidebarOpened = false;
-    document.getElementById("sidebar").style["margin-left"] = "100%";
-  }
+  document.getElementById(position + "-" + "linkedin").onclick = () => {window.open("https://" + linkSosmed["linkedin"] + "/" + userName)};
+  document.getElementById(position + "-" + "github").onclick = () => {window.open("https://" + linkSosmed["github"] + "/" + userName)};
+  document.getElementById(position + "-" + "facebook").onclick = () => {window.open("https://" + linkSosmed["facebook"] + "/" + userName)};
+  document.getElementById(position + "-" + "instagram").onclick = () => {window.open("https://" + linkSosmed["instagram"] + "/" + userName)};
+}
