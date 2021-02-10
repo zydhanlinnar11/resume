d530c2cef0430a6115affba9c5ac12ecf98edb00
Zydhan Linnar Putra <zydhan.19051@mhs.its.ac.id>
2021-02-10 19:12:39 +0700

    Add social media sidebar

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 navButtonPressed();
+let sidebarOpened = false;
 
 function navButtonPressed() {
   let userName = "zydhanlinnar11";
@@ -11,4 +12,19 @@ function navButtonPressed() {
   }
   document.getElementById("linkedin").onclick = function() {window.open("https://" + linkSosmed["linkedin"] + "/" + userName)};
   document.getElementById("gh").onclick = function() {window.open("https://" + linkSosmed["gh"] + "/" + userName)};
+  document.getElementById("sosmed").onclick = function() {
+    if(sidebarOpened) {
+      document.getElementById("sidebar").style["margin-left"] = "100%";
+    } else {
+      document.getElementById("sidebar").style["margin-left"] = "0%";
+    }
+    sidebarOpened = !sidebarOpened;
+  }
+  document.getElementById("linkedin-sidebar").onclick = function() {window.open("https://" + linkSosmed["linkedin"] + "/" + userName)};
+  document.getElementById("gh-sidebar").onclick = function() {window.open("https://" + linkSosmed["gh"] + "/" + userName)};
+  }
+
+  window.onresize = function() {
+    sidebarOpened = false;
+    document.getElementById("sidebar").style["margin-left"] = "100%";
   }
