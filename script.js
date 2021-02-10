0a28f515cc9f23a3d98911e4c33916ba21400879
Zydhan Linnar Putra <zydhan.19051@mhs.its.ac.id>
2021-02-11 05:12:59 +0700

    Add animation
    - Navbar hide animation.
    - Sidebar open/close animation.
    - Hello section fade animation.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 navButtonPressed();
 let sidebarOpened = false;
+let prevYPos = window.pageYOffset;
+playHelloFadeAnim();
+
+window.onscroll = () => {
+  let currYPos = window.pageYOffset;
+  if(currYPos > prevYPos) {
+    document.getElementById("navbar").style.top = "-64px";
+    hideSideBar();
+  } else {
+    document.getElementById("navbar").style.top = "0px";
+  }
+}
 
 window.onresize = () => {
   hideSideBar();
@@ -17,12 +29,32 @@ function navButtonPressed() {
 }
 
 function hideSideBar() {
-  document.getElementById("sidebar").style["margin-left"] = "100%";
+  if(!sidebarOpened)
+    return;
+  let margin = 9;
+  let interval = setInterval(openAnim, 3);
+  function openAnim() {
+    if(margin == -100)
+      clearInterval(interval)
+    else {
+      margin--;
+      document.getElementById("sidebar").style["top"] = margin + "%";
+    }
+  }
   sidebarOpened = false;
 }
 
 function showSideBar() {
-  document.getElementById("sidebar").style["margin-left"] = "0%";
+  let margin = -100;
+  let interval = setInterval(openAnim, 3);
+  function openAnim() {
+    if(margin == 9)
+      clearInterval(interval)
+    else {
+      margin++;
+      document.getElementById("sidebar").style["top"] = margin + "%";
+    }
+  }
   sidebarOpened = true;
 }
 
@@ -39,4 +71,17 @@ function setSosmedLink(position) {
   document.getElementById(position + "-" + "github").onclick = () => {window.open("https://" + linkSosmed["github"] + "/" + userName)};
   document.getElementById(position + "-" + "facebook").onclick = () => {window.open("https://" + linkSosmed["facebook"] + "/" + userName)};
   document.getElementById(position + "-" + "instagram").onclick = () => {window.open("https://" + linkSosmed["instagram"] + "/" + userName)};
+}
+
+function playHelloFadeAnim() {
+  let counter = 0;
+  let interval = setInterval(playAnim, 3);
+  function playAnim() {
+    if(counter == 100) {
+      clearInterval(interval);
+    } else {
+      counter++;
+      document.getElementById("hello-section").style.opacity = (counter / 100);
+    }
+  }
 }
