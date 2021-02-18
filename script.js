0d92733cca6a0e961a2f5d549de9152c505edb39
Zydhan Linnar Putra <zydhan.19051@mhs.its.ac.id>
2021-02-18 08:52:38 +0700

    Add biodata section
    Still have bug blank white space at bottom page.

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
-navButtonPressed();
-let sidebarOpened = false;
-let prevYPos = window.pageYOffset;
-playHelloFadeAnim();
+navButtonPressed(); // Handle navigation button on click
+let sidebarOpened = false; // sideBar hidden when page opened
+playHelloFadeAnim(); // Play fade animation when page opened
 
+// On scroll
+let prevYPos = window.pageYOffset;
 window.onscroll = () => {
+  // Hide navbar when scroll down and vice versa
+  console.log('executed');
   let currYPos = window.pageYOffset;
-  if(currYPos > prevYPos && !sidebarOpened) {
+  if (currYPos > prevYPos && !sidebarOpened) {
     document.getElementById("navbar").style.top = "-64px";
   } else {
     document.getElementById("navbar").style.top = "0px";
   }
-}
+};
 
+// Always hide sidebar when window is resized
 window.onresize = () => {
   hideSideBar();
-}
+};
 
+// set navigation button link
 function navButtonPressed() {
   setSosmedLink("navbar");
   setSosmedLink("sidebar");
   document.getElementById("navbar-sosmed").onclick = () => {
-    if(sidebarOpened)
-      hideSideBar();
-    else
-      showSideBar();
-  }
+    if (sidebarOpened) hideSideBar();
+    else showSideBar();
+  };
 }
 
+// Hide and show sidebar
 function hideSideBar() {
   document.getElementById("sidebar").style["top"] = "-105%";
   sidebarOpened = false;
@@ -37,30 +41,40 @@ function showSideBar() {
   sidebarOpened = true;
 }
 
+// set link for nav/side bar button
 function setSosmedLink(position) {
   let userName = "zydhanlinnar11";
   const linkSosmed = {
-    "twitter" : "twitter.com",
-    "github" : "github.com",
-    "facebook" : "facebook.com",
-    "instagram" : "instagram.com",
-    "linkedin" : "linkedin.com/in"
-  }
-  document.getElementById(position + "-" + "linkedin").onclick = () => {window.open("https://" + linkSosmed["linkedin"] + "/" + userName)};
-  document.getElementById(position + "-" + "github").onclick = () => {window.open("https://" + linkSosmed["github"] + "/" + userName)};
-  document.getElementById(position + "-" + "facebook").onclick = () => {window.open("https://" + linkSosmed["facebook"] + "/" + userName)};
-  document.getElementById(position + "-" + "instagram").onclick = () => {window.open("https://" + linkSosmed["instagram"] + "/" + userName)};
+    twitter: "twitter.com",
+    github: "github.com",
+    facebook: "facebook.com",
+    instagram: "instagram.com",
+    linkedin: "linkedin.com/in",
+  };
+  document.getElementById(position + "-" + "linkedin").onclick = () => {
+    window.open("https://" + linkSosmed["linkedin"] + "/" + userName);
+  };
+  document.getElementById(position + "-" + "github").onclick = () => {
+    window.open("https://" + linkSosmed["github"] + "/" + userName);
+  };
+  document.getElementById(position + "-" + "facebook").onclick = () => {
+    window.open("https://" + linkSosmed["facebook"] + "/" + userName);
+  };
+  document.getElementById(position + "-" + "instagram").onclick = () => {
+    window.open("https://" + linkSosmed["instagram"] + "/" + userName);
+  };
 }
 
+// Fade animation
 function playHelloFadeAnim() {
   let counter = 0;
   let interval = setInterval(playAnim, 3);
   function playAnim() {
-    if(counter == 100) {
+    if (counter == 100) {
       clearInterval(interval);
     } else {
       counter++;
-      document.getElementById("hello-section").style.opacity = (counter / 100);
+      document.getElementById("hello-section").style.opacity = counter / 100;
     }
   }
 }
