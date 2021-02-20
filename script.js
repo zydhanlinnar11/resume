1c941b4b08f2a545a1405110f9ccb80bd3479044
Zydhan Linnar Putra <zydhan.19051@mhs.its.ac.id>
2021-02-20 21:14:30 +0700

    Optimize image load speed.
    - Background image depends on screen resolution.
    - Prettier js and css

--- script.js
+++ script.js
@@ -1,80 +1,80 @@
-navButtonPressed(); // Handle navigation button on click
-let sidebarOpened = false; // sideBar hidden when page opened
-playHelloFadeAnim(); // Play fade animation when page opened
+navButtonPressed() // Handle navigation button on click
+let sidebarOpened = false // sideBar hidden when page opened
+playHelloFadeAnim() // Play fade animation when page opened
 
 // On scroll
-let prevYPos = window.pageYOffset;
+let prevYPos = window.pageYOffset
 window.onscroll = () => {
   // Hide navbar when scroll down and vice versa
-  let currYPos = window.pageYOffset;
+  let currYPos = window.pageYOffset
   if (currYPos > prevYPos && !sidebarOpened) {
-    document.getElementById("navbar").style.top = "-64px";
+    document.getElementById("navbar").style.top = "-64px"
   } else {
-    document.getElementById("navbar").style.top = "0px";
+    document.getElementById("navbar").style.top = "0px"
   }
-  prevYPos = currYPos;
-};
+  prevYPos = currYPos
+}
 
 // Always hide sidebar when window is resized
 window.onresize = () => {
-  hideSideBar();
-};
+  hideSideBar()
+}
 
 // set navigation button link
 function navButtonPressed() {
-  setSosmedLink("navbar");
-  setSosmedLink("sidebar");
+  setSosmedLink("navbar")
+  setSosmedLink("sidebar")
   document.getElementById("navbar-sosmed").onclick = () => {
-    if (sidebarOpened) hideSideBar();
-    else showSideBar();
-  };
+    if (sidebarOpened) hideSideBar()
+    else showSideBar()
+  }
 }
 
 // Hide and show sidebar
 function hideSideBar() {
-  document.getElementById("sidebar").style["top"] = "-105%";
-  sidebarOpened = false;
+  document.getElementById("sidebar").style["top"] = "-105%"
+  sidebarOpened = false
 }
 
 function showSideBar() {
-  document.getElementById("sidebar").style["top"] = "64px";
-  sidebarOpened = true;
+  document.getElementById("sidebar").style["top"] = "64px"
+  sidebarOpened = true
 }
 
 // set link for nav/side bar button
 function setSosmedLink(position) {
-  let userName = "zydhanlinnar11";
+  let userName = "zydhanlinnar11"
   const linkSosmed = {
     twitter: "twitter.com",
     github: "github.com",
     facebook: "facebook.com",
     instagram: "instagram.com",
     linkedin: "linkedin.com/in",
-  };
+  }
   document.getElementById(position + "-" + "linkedin").onclick = () => {
-    window.open("https://" + linkSosmed["linkedin"] + "/" + userName);
-  };
+    window.open("https://" + linkSosmed["linkedin"] + "/" + userName)
+  }
   document.getElementById(position + "-" + "github").onclick = () => {
-    window.open("https://" + linkSosmed["github"] + "/" + userName);
-  };
+    window.open("https://" + linkSosmed["github"] + "/" + userName)
+  }
   document.getElementById(position + "-" + "facebook").onclick = () => {
-    window.open("https://" + linkSosmed["facebook"] + "/" + userName);
-  };
+    window.open("https://" + linkSosmed["facebook"] + "/" + userName)
+  }
   document.getElementById(position + "-" + "instagram").onclick = () => {
-    window.open("https://" + linkSosmed["instagram"] + "/" + userName);
-  };
+    window.open("https://" + linkSosmed["instagram"] + "/" + userName)
+  }
 }
 
 // Fade animation
 function playHelloFadeAnim() {
-  let counter = 0;
-  let interval = setInterval(playAnim, 3);
+  let counter = 0
+  let interval = setInterval(playAnim, 3)
   function playAnim() {
     if (counter == 100) {
-      clearInterval(interval);
+      clearInterval(interval)
     } else {
-      counter++;
-      document.getElementById("hello-section").style.opacity = counter / 100;
+      counter++
+      document.getElementById("hello-section").style.opacity = counter / 100
     }
   }
 }
