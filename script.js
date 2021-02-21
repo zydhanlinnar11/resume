c5a8395febcfa7d19bd5de00702fde9d75624e11
Zydhan Linnar Putra <zydhan.19051@mhs.its.ac.id>
2021-02-21 17:51:21 +0700

    Rename tag and simplify .js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-navButtonPressed() // Handle navigation button on click
+setNavButtonOnClick() // Handle navigation button on click
 let sidebarOpened = false // sideBar hidden when page opened
 playHelloFadeAnim() // Play fade animation when page opened
 
@@ -6,22 +6,23 @@ playHelloFadeAnim() // Play fade animation when page opened
 let prevYPos = window.pageYOffset
 window.onscroll = () => {
   // Hide navbar when scroll down and vice versa
-  let currYPos = window.pageYOffset
-  if (currYPos > prevYPos && !sidebarOpened) {
-    document.getElementById("navbar").style.top = "-64px"
-  } else {
-    document.getElementById("navbar").style.top = "0px"
-  }
-  prevYPos = currYPos
+  if (isScrolledDown() && !sidebarOpened) hideNavbar()
+  else showNavbar()
+  prevYPos = window.pageYOffset
 }
+
+// Navbar hide / show function
+function hideNavbar() { document.getElementById("navbar").style.top = "-64px" }
+function showNavbar() { document.getElementById("navbar").style.top = "0px" }
+
+// isScrolledDown function
+let isScrolledDown = () => window.pageYOffset > prevYPos
 
 // Always hide sidebar when window is resized
-window.onresize = () => {
-  hideSideBar()
-}
+window.onresize = () => { hideSideBar() }
 
 // set navigation button link
-function navButtonPressed() {
+function setNavButtonOnClick() {
   setSosmedLink("navbar")
   setSosmedLink("sidebar")
   document.getElementById("navbar-sosmed").onclick = () => {
@@ -45,36 +46,30 @@ function showSideBar() {
 function setSosmedLink(position) {
   let userName = "zydhanlinnar11"
   const linkSosmed = {
-    twitter: "twitter.com",
     github: "github.com",
     facebook: "facebook.com",
     instagram: "instagram.com",
     linkedin: "linkedin.com/in",
   }
-  document.getElementById(position + "-" + "linkedin").onclick = () => {
-    window.open("https://" + linkSosmed["linkedin"] + "/" + userName)
-  }
-  document.getElementById(position + "-" + "github").onclick = () => {
-    window.open("https://" + linkSosmed["github"] + "/" + userName)
-  }
-  document.getElementById(position + "-" + "facebook").onclick = () => {
-    window.open("https://" + linkSosmed["facebook"] + "/" + userName)
-  }
-  document.getElementById(position + "-" + "instagram").onclick = () => {
-    window.open("https://" + linkSosmed["instagram"] + "/" + userName)
+  for (let key in linkSosmed) setOnClick(linkSosmed, position, key, userName)
+}
+
+function setOnClick(linkSosmed, position, sosmed, userName) {
+  document.getElementById(`${position}-${sosmed}`).onclick = () => {
+    window.open(`https://${linkSosmed[sosmed]}/${userName}`)
   }
 }
 
 // Fade animation
 function playHelloFadeAnim() {
+  const animationTimeInMs = 1000
+  const FRAME_TIME = 16 // 60 FPS
+  const MAX_COUNTER = animationTimeInMs / FRAME_TIME
   let counter = 0
-  let interval = setInterval(playAnim, 3)
-  function playAnim() {
-    if (counter == 100) {
-      clearInterval(interval)
-    } else {
-      counter++
-      document.getElementById("hello-section").style.opacity = counter / 100
-    }
-  }
+  let interval = setInterval(() => {
+    if (counter == MAX_COUNTER) clearInterval(interval)
+    else
+      counter++,
+        (document.getElementById("hello-section").style.opacity = counter / MAX_COUNTER)
+  }, FRAME_TIME)
 }
